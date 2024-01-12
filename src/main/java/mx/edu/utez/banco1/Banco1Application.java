30bbf269a99de472aa0df365ff4ea4fffe2e3ca4
Jhafet19 <20223tn107@utez.edu.mx>
2024-01-12 08:16:07 -0600

    Primer Commit

---
+++ src/main/java/mx/edu/utez/banco1/Banco1Application.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.banco1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Banco1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Banco1Application.class, args);
+	}
+
+}
---
+++ src/main/java/mx/edu/utez/banco1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.banco1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Banco1Application.class);
+	}
+
+}
